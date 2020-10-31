f8464aee18d6f3b65f4f2968a216d5c0b5a1586c
vguadalupe <44405688+vguadalupe@users.noreply.github.com>
2020-10-30 22:01:46 -0500

    Add project files.

---
+++ AdrianGuadalupe_examen/AdrianGuadalupe_examen/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AdrianGuadalupe_examen
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            Login = new Login();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
